47821115293335c6092234a52604b14da0ee0c3e
Jiří Vrba <jiri@vrba.dev>
2020-12-02 18:54:45 +0100

    Added 2020/01

--- src/main/scala/dev/vrba/adventofcode/AdventOfCode.scala
+++ src/main/scala/dev/vrba/adventofcode/AdventOfCode.scala
@@ -2,5 +2,6 @@ package dev.vrba.adventofcode
 
 object AdventOfCode {
   def main(args: Array[String]): Unit = {
+    // 1. 12. 2020 new dev.vrba.adventofcode.day1.ReportRepair().execute(SecondPart)
   }
 }
--- src/main/scala/dev/vrba/adventofcode/day1/ReportRepair.scala
+++ src/main/scala/dev/vrba/adventofcode/day1/ReportRepair.scala
@@ -1,9 +1,46 @@
 package dev.vrba.adventofcode.day1
 
-import dev.vrba.adventofcode.{AdventOfCodeTask, AdventOfCodeTaskSolution, FirstPart, TaskPart}
+import dev.vrba.adventofcode.helpers.ResourceLoader
+import dev.vrba.adventofcode.{AdventOfCodeTask, AdventOfCodeTaskSolution, FirstPart, SecondPart, TaskPart}
+
+import scala.Option
+import scala.collection.mutable
 
 @AdventOfCodeTask(2019, 1)
 class ReportRepair extends AdventOfCodeTaskSolution {
   override def execute(part: TaskPart): Unit = {
+    val input = ResourceLoader.readResourceLines("/2020/01/input")
+      .map(_.toInt)
+      .toSet
+
+    part match {
+      case FirstPart => findMatchingTuple(input, 2020).foreach(result => println(s"Result: ${result._1 * result._2}"))
+      case SecondPart => findMatchingTriple(input, 2020).foreach(result => println(s"Result: ${result._1 * result._2 * result._3}"))
+    }
+  }
+
+  def findMatchingTuple(set: Set[Int], target: Int): Option[(Int, Int)] = {
+    val complements = mutable.Set[Int]()
+
+    set.foreach { value =>
+      if (complements.contains(target - value)) return Some(value, target - value)
+      else complements.add(value)
+    }
+
+    None
+  }
+
+  def findMatchingTriple(set: Set[Int], target: Int): Option[(Int, Int, Int)] = {
+    set.zipWithIndex.foreach(entry => {
+      val (value, index) = entry
+      val subset = set.drop(index)
+
+      findMatchingTuple(subset, target - value) match {
+        case Some(result) => return Some(value, result._1, result._2)
+        case None =>
+      }
+    })
+
+    None
   }
 }
