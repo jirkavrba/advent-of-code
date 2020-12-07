4ffdc69b1f224e4ce3dcab797cbc6baca42893f6
Jiří Vrba <jiri@vrba.dev>
2020-12-07 08:27:43 +0100

    Preparation for 2020/07 solution

---
+++ src/main/scala/dev/vrba/adventofcode/day7/HandyHaversacks.scala
@@ -0,0 +1,8 @@
+package dev.vrba.adventofcode.day7
+
+import dev.vrba.adventofcode.{AdventOfCodeTask, AdventOfCodeTaskSolution, TaskPart}
+
+@AdventOfCodeTask(2020, 7)
+class HandyHaversacks extends AdventOfCodeTaskSolution {
+  override def execute(part: TaskPart): Unit = ???
+}
